3e4d2c360ffcdd38c9626a86552f9ff39276fb5a
김상두 <puki4416@gmail.com>
2022-10-28 22:08:18 +0900

    프로토타입 패턴 제작

---
+++ 생성패턴/prototype.js
@@ -0,0 +1,27 @@
+class longTimeWork {
+  flag = false;
+  text;
+  constructor() {
+    setTimeout(() => {
+      // 3초후에 텍스트 입력가능
+      this.flag = true;
+    }, 3000);
+  }
+  setText(text) {
+    if (this.flag) {
+      this.text = text;
+      console.log(this.text);
+    }
+  }
+}
+
+const main = () => {
+  const work1 = new longTimeWork();
+  setTimeout(() => {
+    work1.setText("test1");
+    const work2 = Object.create(work1);
+    work2.setText("test2");
+  }, 4000);
+};
+
+main();
